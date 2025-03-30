51f6c5cca8f4f1ccb355e60500717153bfe41701
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2025-03-30 20:31:50 +0200

    Add bulma CSS

--- front/src/components/Videos.vue
+++ front/src/components/Videos.vue
@@ -11,17 +11,33 @@ function get_videos() {
 </script>
 
 <template>
-    <section>
-        <input type="text" v-model="game" placeholder="Enter a game name"/>
-        <button @click="get_videos">Search</button>
-        <p v-if="loading">Loading...</p>
-        <p v-if="error">{{ error }}</p>
-        <ol v-if="data">
-            <li v-for="video in data" :key="video.id">
-                {{ video.title }}
-                <img :src="video.thumbnail_url.replace('%{width}', '480').replace('%{height}', '234')" alt="{{ video.title }}" loading="lazy">
-                <a :href="video.url"></a>
-            </li>
-        </ol>
+    <section class="section">
+        <form action="javascript:void(0);">
+            <div class="field">
+                <label class="label">Game name</label>
+                <input type="text" v-model="game" placeholder="Enter a game name" class="input"/>
+                <button @click="get_videos" class="button" type="submit">Search</button>
+            </div>
+        </form>
+        <div class="section">
+            <p v-if="loading" class="notification is-info">Loading...</p>
+            <p v-if="error" class="notification is-danger">{{ error }}</p>
+            <div v-if="data" class="grid is-col-min-12">
+                <div v-for="video in data" :key="video.id" class="cell">
+                    <div class="card">
+                        <div class="card-image">
+                            <a :href="video.url" target="_blank">
+                                <figure class="image is-16by9">
+                                    <img :src="video.thumbnail_url.replace('%{width}', '640').replace('%{height}', '360')" alt="{{ video.title }}" loading="lazy" onclick="">
+                                </figure>
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <p class="content">{{ video.title }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
